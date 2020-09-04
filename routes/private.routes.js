ea7d2edb7d2e777dadc1ce00d1c05a514de38b6b
Christian Martinez <Christian.Martinez@udr.com>
2020-09-04 11:33:33 -0600

    (payment/7) - Add middleware validation: - Middleware added to generate card - Set env var for port - Cleaned the package.json

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const cardController = require('../controllers/card.controller');
+const validation = require('../utils/validations')
 
-// GET /error
-router.post('/pay', cardController.postTransaction );
-router.post('/card', cardController.postCreditCard );
+router.post('/pay', validation.pay, cardController.postTransaction);
+router.post('/card', cardController.postCreditCard);
 
 module.exports = {routes: router};
